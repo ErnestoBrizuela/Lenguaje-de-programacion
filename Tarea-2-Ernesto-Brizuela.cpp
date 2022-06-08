36288a47c81855523a07582666ff81ea191f8873
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-08 10:44:07 -0600

    Add files via upload

---
+++ Tarea-2-Ernesto-Brizuela.cpp
@@ -0,0 +1,50 @@
+/*
+		Propósito: Calculo de Factura con Descuento
+		
+		Autor: Ernesto Brizuela
+		Fecha: 08/06/2022
+		Hora: 10:00 am
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main()
+ {
+	 setlocale(LC_CTYPE, "Spanish");
+	 cout << setprecision(10);
+	 
+	 double subtotal = 0, total = 0, calculoDescuento = 0, calculoImpuesto = 0;
+	 int descuento = 0;
+	 char estaExenta;
+	 
+	 cout << "Ingrese el subtotal de la factura: ";
+	 cin >> subtotal;
+	 
+	 cout << "Ingrese el descuento (0, 10, 15, 20): ";
+	 cin >> descuento;
+	 
+	 cout << "¿La factura está exenta? S o N: ";
+	 cin >> estaExenta;
+	 
+	 if(estaExenta == 'S'|| estaExenta == 's')
+	 {
+	  calculoDescuento = (subtotal * descuento) / 100;
+	  total = subtotal - calculoDescuento;
+	 }
+	 else
+	 {	  
+	  calculoDescuento = (subtotal * descuento) / 100;
+	  calculoImpuesto = (subtotal - calculoDescuento) * 0.15;
+	  total = subtotal - calculoDescuento + calculoImpuesto;	 
+	 }
+	 
+	 cout << "El total a pagar es: " << total;
+
+	 return 0;
+	 
+ }
+
