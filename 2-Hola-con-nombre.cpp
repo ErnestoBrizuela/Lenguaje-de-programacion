a67012866ae60a3d62df1f35621819b77c3e553e
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 10:12:38 -0600

    Add files via upload

---
+++ 2-Hola-con-nombre.cpp
@@ -0,0 +1,29 @@
+/*
+		Propósito: 2. Hola con Nombre
+		
+		Autor: Ernesto Brizuela
+		Fecha: 29/06/2022
+		Hora: 10:00 AM
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char const *argv[])
+ {
+	 string nombre;
+	 
+	 cout << "Ingrese el nombre: ";
+	 cin >> nombre;
+	 
+	 cout << endl;
+	 cout << "Su nombre es: " << nombre;
+	 cout << endl;
+	 
+	 return 0;
+	 
+ }
+
