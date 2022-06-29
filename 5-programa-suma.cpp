663febb74d9e66c5cb063b80f0748b0b3b8b201d
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 11:07:57 -0600

    Add files via upload

---
+++ 5-programa-suma.cpp
@@ -0,0 +1,36 @@
+/*
+		Propósito:5. Programa Suma
+		
+		Autor: Ernesto Brizuela
+		Fecha: 29/06/2022
+		Hora: 10:00 AM
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char** argv)
+ {
+	 int a = 0;
+	 int b = 0;
+	 int resultado = 0;
+	 
+	 cout << "Ingrese el valor de a: ";
+	 cin >> a;
+	 
+	 cout << endl;
+	 
+	 cout << "Ingrese el valor de b: ";
+	 cin >> b;
+	 
+	 resultado = a + b;
+	 
+	 cout << "La suma de a + b es: " << resultado;
+
+	 return 0;
+	 
+ }
+
