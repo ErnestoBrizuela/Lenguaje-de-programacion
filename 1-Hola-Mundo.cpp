0cdfee31546156a26cffc6acbf26eee3b330bd95
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 10:08:18 -0600

    Add files via upload

---
+++ 1-Hola-Mundo.cpp
@@ -0,0 +1,22 @@
+/*
+		Propósito: 1. Hola Mundo
+		
+		Autor: Ernesto Brizuela
+		Fecha: 29/06/2022
+		Hora: 10:00 AM
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char const *argv[])
+ {
+	 std::cout << "Hola Mundo!";
+	 
+	 return 0;
+	 
+ }
+
