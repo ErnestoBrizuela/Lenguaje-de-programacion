0a8b18039922ce40282074682d3d29a8a5c9f26d
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 16:17:02 -0600

    Add files via upload

---
+++ 18-ciclos-for.cpp
@@ -0,0 +1,25 @@
+/*
+		Propósito: 18. Ciclos For
+		
+		Autor: Ernesto Brizuela
+		Fecha: 29/06/2022
+		Hora: 3:00 PM
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char** argv)
+ {
+	for(int indice = 1; indice <= 10; indice = indice + 1){
+		cout << "Valor de la variable indice:  " << indice << endl;
+	}
+	 
+
+	 return 0;
+	 
+ }
+
