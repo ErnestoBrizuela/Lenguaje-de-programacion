985cc21e59362aa17df84187b0353a148ec2986b
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 12:45:44 -0600

    Add files via upload

---
+++ 13-ciclos-infinitos.cpp
@@ -0,0 +1,29 @@
+/*
+		Propósito:
+		
+		Autor:
+		Fecha:
+		Hora:
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char const *argv[])
+ {
+	 int ciclos = 0;
+	 
+	 while(ciclos < 1000000){
+	 	cout << "*";
+	 	
+	 	ciclos = ciclos + 1;
+	 }
+	 
+
+	 return 0;
+	 
+ }
+
