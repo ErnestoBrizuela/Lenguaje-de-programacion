6bf446f46b70adb36d882b8c9c5238acdb378212
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-06-29 10:32:48 -0600

    Add files via upload

---
+++ 3-Variables.cpp
@@ -0,0 +1,29 @@
+/*
+		Propósito: 3. Variables
+		
+		Autor: Ernesto Brizuela
+		Fecha: 29/06/2022
+		Hora: 10:00 AM
+*/
+ 
+ #include <iostream>
+ #include <cmath>
+ #include <iomanip>
+ 
+ using namespace std;
+ 
+ int main(int argc, char const *argv[])
+ {
+	 int numeroEntero;
+	 numeroEntero = 15;	 
+	 
+	 cout << "Entero: " << numeroEntero << endl;
+	 
+	 numeroEntero = 18;
+	 
+	 cout << "Entero modificado: " << numeroEntero;
+	 
+	 return 0;
+	 
+ }
+
