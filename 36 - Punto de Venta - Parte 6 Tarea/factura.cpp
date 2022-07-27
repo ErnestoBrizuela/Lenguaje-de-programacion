fa26f9ca46a2a300e01ed82518ad91528fa35f7c
ErnestoBrizuela <106693341+ErnestoBrizuela@users.noreply.github.com>
2022-07-27 16:40:06 -0600

    Add files via upload

---
+++ 36 - Punto de Venta - Parte 6 Tarea/factura.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+double impuesto;
+double subtotal;
+double total;
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+{
+	listaProductos = listaProductos + descripcion + "\n";
+	subtotal = subtotal + (cantidad * precio);
+	impuesto = subtotal * 0.15;
+	total = subtotal + impuesto;
+	
+}
+
+void imprimirFactura()
+{
+	system("cls");
+	cout << "*******" << endl;
+	cout << "FACTURA" << endl;
+	cout << "*******" << endl;
+	
+	cout << "Productos: " << endl;
+	cout << listaProductos;
+	
+	cout << endl;
+	cout << "Subtotal: " << subtotal;
+	cout << endl;
+	
+	cout << "Impuesto: " << impuesto;
+	cout << endl;
+	
+	cout << "Total: " << total;
+	cout << endl;
+	system("pause");
+}
